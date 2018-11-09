d7d94a0e5a4a61f2658fff2b2f794cef173ca5b6
shinto <shinto@vofoxsolutions.com>
2018-11-09 10:08:12 +0530

    09/11/2018 commit by shinto 10:08 AM

--- Scripts/Associate Application/Associate App Production/TC5/Script1541595389649.groovy
+++ Scripts/Associate Application/Associate App Production/TC5/Script1541595389649.groovy
@@ -3,28 +3,19 @@ import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
 import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
-import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
 import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 
 WebUI.verifyElementText(findTestObject('Object Repository/Customer Account/Page_Home  5-15/span_My Title'), 'My Title:')
 
 WebUI.verifyElementText(findTestObject('Object Repository/Customer Account/Page_Home  5-15/p_My Customers'), 'My Customers')
-
-WebUI.verifyElementText(findTestObject('Object Repository/Customer Account/Page_Home  5-15/span_Add Customer'), 'Add Customer')
 
 WebUI.click(findTestObject('Object Repository/Customer Account/Page_Home  5-15/span_Add Customer'))
 
@@ -78,9 +69,6 @@ WebUI.setText(findTestObject('Object Repository/Customer Account/Page_My Custome
 WebUI.setText(findTestObject('Object Repository/Customer Account/Page_My Customers  5-15/textarea_form-control au-targe'), 
     'Test')
 
-WebUI.verifyElementText(findTestObject('Object Repository/Customer Account/Page_My Customers  5-15/span_Next -- Add Utilty Data'), 
-    'Next -- Add Utilty Data')
-
 WebUI.click(findTestObject('Object Repository/Customer Account/Page_My Customers  5-15/span_Next -- Add Utilty Data'))
 
 String data = CustomKeywords.'com.Baseclass.Accountnumber.generateRandomData'()
@@ -109,8 +97,6 @@ WebUI.click(findTestObject('Object Repository/Customer Account/Page_My Customers
 WebUI.click(findTestObject('Object Repository/Customer Account/Page_My Customers  5-15/span_Order Confirmation Page'))
 
 WebUI.click(findTestObject('Object Repository/Customer Account/Page_My Customers  5-15/input_au-target'))
-
-WebUI.verifyElementText(findTestObject('Object Repository/Customer Account/Page_My Customers  5-15/button_Submit'), 'Submit')
 
 WebUI.click(findTestObject('Object Repository/Customer Account/Page_My Customers  5-15/button_Submit'))
 
