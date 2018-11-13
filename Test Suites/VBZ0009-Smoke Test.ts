474775189984d204fbc135a0cef9ae64a58c5076
Manoj <manoj@vofoxsolutions.com>
2018-11-13 11:49:58 +0530

    13/11/18

--- Test Suites/VBZ0009-Smoke Test.ts
+++ Test Suites/VBZ0009-Smoke Test.ts
@@ -4,7 +4,7 @@
    <name>VBZ0009-Smoke Test</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-11-12T18:27:02</lastRun>
+   <lastRun>2018-11-13T10:32:12</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
