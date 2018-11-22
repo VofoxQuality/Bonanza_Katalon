879d0119703cc8f4a21d1d15602db073cac5166b
VOFOX\sebastian <sebansoney@gmail.com>
2018-11-22 14:50:18 +0530

    15:50

--- Object Repository/Executive/Page_Home  5-15/a_Executive.rs
+++ Object Repository/Executive/Page_Home  5-15/a_Executive.rs
@@ -4,7 +4,17 @@
    <name>a_Executive</name>
    <tag></tag>
    <elementGuidId>f6ecbff5-d3ca-4db9-9de0-5ffcfaa1250c</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[contains(text(),'Executive')]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//a[@id = 'lnk_2' and @href = '#/exec' and (text() = ' Executive' or . = ' Executive')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
--- Test Suites/VBZ0020-Page Management.ts
+++ Test Suites/VBZ0020-Page Management.ts
@@ -4,7 +4,7 @@
    <name>VBZ0020-Page Management</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-10-26T16:26:33</lastRun>
+   <lastRun>2018-11-22T12:30:26</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
